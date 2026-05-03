43a637f77ea3a0038908d1ec65789f88aa5f74b6
GOR <greatestorrichest@gmail.com>
2026-05-03 15:05:33 +0000

    <meta name="google-site-verification" content="T7MJ2sPEwRPxrR_PAQGUkHXiA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export const metadata: Metadata = {
     images: ['/og-image.png'],
   },
   verification: {
-    google: 'googlef11e306c0ec883d4',
+    google: 'T7MJ2sPEwRPxrR_PAQGUkHXiAFt0VllUH1gFNMDda-A',
   },
   robots: {
     index: true,
@@ -127,7 +127,7 @@ export default function RootLayout({
         <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-K2K5DGZB');`}
         </Script>
